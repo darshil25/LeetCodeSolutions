448002f251bf3daa99992e83a9aecd571c5f65ef
Darshil Thummar <thummardarshil9687@gmail.com>
2023-05-21 18:34:23 +0530

    Update Q4 Median of Two Sorted Arrays.java

--- Q4 Median of Two Sorted Arrays.java
+++ Q4 Median of Two Sorted Arrays.java
@@ -43,5 +43,15 @@ class Solution {
     }
 }
 /*
+This code works by first merging the two sorted arrays into a single array. The median is then calculated by finding the middle element of the merged array, or the average of the two middle elements if the number of elements is even.
 
+Here are some additional details about the code:
+
+The findMedianSortedArrays() method takes two sorted arrays as input and returns the median of the two arrays.
+The n and m variables store the lengths of the two arrays.
+The merge array is used to store the merged elements of the two arrays.
+The i and j variables are pointers to the current positions in the nums1 and nums2 arrays, respectively.
+The k variable is used to keep track of the current position in the merge array.
+The while loops iterate over the two arrays, adding the smaller element to the merge array.
+The if statement checks if the number of elements in the merge array is odd. If it is, the median is the middle element. If it is even, the median is the average of the two middle elements.
 */
