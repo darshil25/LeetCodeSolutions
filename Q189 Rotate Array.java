2dd8bebab779e46d1796cd3d3ec846f06035f5a3
Darshil Thummar <thummardarshil9687@gmail.com>
2023-07-12 22:07:25 +0530

    Create Q189 Rotate Array.java

---
+++ Q189 Rotate Array.java
@@ -0,0 +1,20 @@
+class Solution {
+    public void rotate(int[] nums, int k) {
+        int N = nums.length;
+        k = k % N;
+        reverse(nums, 0, N-1);
+        reverse(nums , 0 , k-1);
+        reverse(nums , k, N-1);
+        System.gc();    
+    }
+    public void reverse(int nums[] , int start, int end){
+        
+        while(start < end){
+            int temp = nums[start];
+            nums[start] = nums[end];
+            nums[end] = temp;
+            start++;
+            end--;
+        }
+    }
+}
